984b22fbf425771f3a6aec6136d507cba9ca7f57
gayoung <gayoung990911@gmail.com>
2025-07-23 14:34:41 +0900

    feat: 즐겨찾기 기능을 위한 DI 의존성 추가
    - AppModule에 FavoriteRepository 의존성 등록

--- composeApp/src/commonMain/kotlin/net/ifmain/hwanultoktok/kmp/di/AppModule.kt
+++ composeApp/src/commonMain/kotlin/net/ifmain/hwanultoktok/kmp/di/AppModule.kt
@@ -1,20 +1,23 @@
 package net.ifmain.hwanultoktok.kmp.di
 
-import org.koin.core.module.dsl.factoryOf
-import org.koin.core.module.dsl.singleOf
-import org.koin.dsl.bind
-import org.koin.dsl.module
 import net.ifmain.hwanultoktok.kmp.data.remote.KoreaExImBankApi
 import net.ifmain.hwanultoktok.kmp.data.repository.AlertRepositoryImpl
 import net.ifmain.hwanultoktok.kmp.data.repository.ExchangeRateRepositoryImpl
+import net.ifmain.hwanultoktok.kmp.data.repository.FavoriteRepositoryImpl
 import net.ifmain.hwanultoktok.kmp.domain.repository.AlertRepository
 import net.ifmain.hwanultoktok.kmp.domain.repository.ExchangeRateRepository
+import net.ifmain.hwanultoktok.kmp.domain.repository.FavoriteRepository
+import net.ifmain.hwanultoktok.kmp.domain.usecase.CheckIsFavoriteUseCase
 import net.ifmain.hwanultoktok.kmp.domain.usecase.GetAlertsUseCase
 import net.ifmain.hwanultoktok.kmp.domain.usecase.GetExchangeRatesUseCase
+import net.ifmain.hwanultoktok.kmp.domain.usecase.GetFavoritesUseCase
 import net.ifmain.hwanultoktok.kmp.domain.usecase.ManageAlertUseCase
 import net.ifmain.hwanultoktok.kmp.domain.usecase.RefreshExchangeRatesUseCase
-import net.ifmain.hwanultoktok.kmp.presentation.viewmodel.ExchangeRateViewModel
+import net.ifmain.hwanultoktok.kmp.domain.usecase.ToggleFavoriteUseCase
 import net.ifmain.hwanultoktok.kmp.presentation.viewmodel.AlertViewModel
+import net.ifmain.hwanultoktok.kmp.presentation.viewmodel.ExchangeRateViewModel
+import org.koin.core.module.dsl.factoryOf
+import org.koin.dsl.module
 
 val commonModule = module {
     single { createHttpClient() }
@@ -23,11 +26,15 @@ val commonModule = module {
     
     single<ExchangeRateRepository> { ExchangeRateRepositoryImpl(get(), get()) }
     single<AlertRepository> { AlertRepositoryImpl(get()) }
+    single<FavoriteRepository> { FavoriteRepositoryImpl(get()) }
     
     factoryOf(::GetExchangeRatesUseCase)
     factoryOf(::RefreshExchangeRatesUseCase)
     factoryOf(::GetAlertsUseCase)
     factoryOf(::ManageAlertUseCase)
+    factoryOf(::GetFavoritesUseCase)
+    factoryOf(::ToggleFavoriteUseCase)
+    factoryOf(::CheckIsFavoriteUseCase)
     
     factoryOf(::ExchangeRateViewModel)
     factoryOf(::AlertViewModel)
