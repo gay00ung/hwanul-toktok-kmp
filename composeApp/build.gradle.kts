e585591a334c3c2339ce5891459ae558f3267cd3
gayoung <gayoung990911@gmail.com>
2025-07-24 13:53:54 +0900

    fix: iOS 빌드 문제 해결

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -31,6 +31,17 @@ kotlin {
     }
     
     sourceSets {
+        val commonMain by getting
+        val androidMain by getting
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+        }
         
         androidMain.dependencies {
             implementation(compose.preview)
