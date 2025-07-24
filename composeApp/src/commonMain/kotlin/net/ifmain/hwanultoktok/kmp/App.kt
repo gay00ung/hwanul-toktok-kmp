75f070db55d8fc5ff1808a56ed5aa2e3f5cbf76a
gayoung <gayoung990911@gmail.com>
2025-07-24 09:19:59 +0900

    chore: 사용하지 않는 import문 제거

--- composeApp/src/commonMain/kotlin/net/ifmain/hwanultoktok/kmp/App.kt
+++ composeApp/src/commonMain/kotlin/net/ifmain/hwanultoktok/kmp/App.kt
@@ -3,21 +3,30 @@ package net.ifmain.hwanultoktok.kmp
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AttachMoney
 import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.ui.tooling.preview.Preview
-import org.koin.compose.KoinApplication
-import org.koin.compose.koinInject
 import net.ifmain.hwanultoktok.kmp.di.commonModule
 import net.ifmain.hwanultoktok.kmp.di.platformModule
 import net.ifmain.hwanultoktok.kmp.presentation.theme.HwanulTheme
-import net.ifmain.hwanultoktok.kmp.presentation.ui.ExchangeRateScreen
 import net.ifmain.hwanultoktok.kmp.presentation.ui.AlertScreen
+import net.ifmain.hwanultoktok.kmp.presentation.ui.ExchangeRateScreen
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.KoinApplication
 
 @Composable
 @Preview
