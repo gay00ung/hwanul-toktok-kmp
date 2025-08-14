2d46b4edb403ac50949b2d7fb6efcfd2bc73103a
gayoung <gayoung990911@gmail.com>
2025-08-14 13:55:49 +0900

    Bump app version to 1.0.2 and update versionCode

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -131,8 +131,8 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         //noinspection OldTargetApi
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
         
         // Load API key from local.properties
         val localProperties = Properties()
